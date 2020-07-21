3af0cf8c1e34619f425e38f654e1e2e016102e31
Gabriel <gbordesc@uft.edu>
2020-07-21 16:49:03 -0400

    LISTO

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
 
 class DataSearch extends SearchDelegate{
   String seleccion='';
@@ -11,6 +13,7 @@ class DataSearch extends SearchDelegate{
     'Steve Jobs'
   ];
 
+  final peliculasProvider= new PeliculasProvider();
 
   final peliculasRecientes=[
     'Spiderman',
@@ -63,27 +66,73 @@ class DataSearch extends SearchDelegate{
   @override
   Widget buildSuggestions(BuildContext context) {
     //Sugerencias que aparecen al escribir
+    
+    if (query.isEmpty){
+      return Container();
+    }
+    
+    return FutureBuilder(
+        future: peliculasProvider.buscarPelicula(query),
+        builder: (BuildContext context, AsyncSnapshot<List<Pelicula>>snapshot){
+          if(snapshot.hasData){
+
+            final peliculas=snapshot.data;
+            return ListView(
+              children:peliculas.map((pelicula){
+                return ListTile(
+                  leading: FadeInImage(
+                    image: NetworkImage(pelicula.getPosterImg()),
+                    placeholder: AssetImage('assets/img/no-image.jpg'),
+                    fit: BoxFit.contain,
+                  ),
+                  title: Text(pelicula.title),
+                  subtitle: Text(pelicula.originalTitle),
+                  onTap: (){
+                    close(context,null);
+                    pelicula.uniqueId='';
+                    Navigator.pushNamed(context, 'detalle',arguments: pelicula);
+                  },
+                );
+              }).toList()
+            );
+          }else{
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+
+          }
+
+
+    }
 
 
 
-    final listaSugerida=(query.isEmpty)
-                      ?peliculasRecientes
-                      :peliculas.where((element) => element.toLowerCase().startsWith(query)).toList();
-    return ListView.builder(
-        itemBuilder: (context, i) {
-          return ListTile(
-            leading: Icon(Icons.movie),
-            title: Text(listaSugerida[i]),
-            onTap: () {
-              seleccion=listaSugerida[i];
-              showResults(context);
-            },
-
-          );
-        },
-      itemCount: listaSugerida.length,
     );
+
   }
 
 
 }
+
+
+//
+//
+//
+//  final listaSugerida=(query.isEmpty)
+//      ?peliculasRecientes
+//      :peliculas.where((element) => element.toLowerCase().startsWith(query)).toList();
+//  return ListView.builder(
+//  itemBuilder: (context, i) {
+//  return ListTile(
+//  leading: Icon(Icons.movie),
+//  title: Text(listaSugerida[i]),
+//  onTap: () {
+//  seleccion=listaSugerida[i];
+//  showResults(context);
+//  },
+//
+//  );
+//  },
+//  itemCount: listaSugerida.length,
+//  );
+//}
